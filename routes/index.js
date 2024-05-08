9eee4fd94ea7ee767fdcd48aa90633ca9130f1d3
Simi oluwatomi <deifilius1@gmail.com>
2024-05-08 12:59:13 +0100

    feat: make index page dynamic

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('blog', { title: 'GTCO Food and Drink' });
+  let navigationLinks = [
+      'Multiplex PCR', 'Technology', 'Design', 'Culture', 'Business', 'Sports'
+  ];
+
+  let rawdata = fs.readFileSync('./database/posts.json');
+
+  let posts = JSON.parse(rawdata);
+
+  let featuredPosts = posts.filter('is_featured')
+
+  res.render('blog', {
+    title: 'She Code Queens',
+    links: navigationLinks,
+    posts: posts
+  });
+
 });
 
 module.exports = router;
