4a45f6427dec3063ec08285740188bf540ce9177
Simi oluwatomi <deifilius1@gmail.com>
2024-05-12 20:13:45 +0100

    refactor: :lipstick: improve the home page

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,6 @@
 let mix = require('laravel-mix');
 
-// mix.copy('node_modules/bootstrap/dist/css/bootstrap.css', 'public/css/index.css');
-
-mix.sass('resources/sass/index.scss', 'public/css/app.css')
+mix.sass('resources/sass/app.scss', 'public/css/app.css')
     .js('resources/js/index.js', 'public/js')
     .copy('resources/sass/index.css', 'public/css/')
     .copy('resources/sass/blog.css', 'public/css/');
